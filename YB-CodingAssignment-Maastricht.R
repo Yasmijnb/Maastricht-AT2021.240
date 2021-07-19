4c12c346bb0c1c3cfcb7f68acce843ef2f33959f
Yasmijnb <yasmijn.balder@hotmail.com>
2021-07-19 14:16:01 +0200

    Finished step 1 and 2

--- YB-CodingAssignment-Maastricht.R
+++ YB-CodingAssignment-Maastricht.R
@@ -1,33 +1,92 @@
 ################################################################################
 
+# Yasmijn Balder
 # Coding assignment for PhD position AT2021.240 in Maastricht
-
-################################################################################
-
-# Install packages
-
-if (!requireNamespace("BiocManager", quietly = TRUE))
-  install.packages("BiocManager")
-
-BiocManager::install("RCy3")
-BiocManager::install("biomaRt")
 
 ################################################################################
 
 # Import packages
 library(biomaRt)
 library(RCy3)
-
-################################################################################
+library(stringr)
 
 # Import data
-
-variants <- read.csv("./variant_list.txt", header = FALSE)
+variants <- read.csv("./variant_list.txt", col.names = 'rsID', header = FALSE)
 
 ################################################################################
 
-# Step 1: Retrieve the associated gene for each variant using BioMart
+## Step 1: Retrieve the associated gene for each variant using BioMart
 
+# Create empty column to store the associated genes
+variants$gene <- rep('', nrow(variants))
 
+# Create bioMart database
+listEnsembl()
+variation = useEnsembl(biomart = "snps")
+listDatasets(variation)
+variation = useEnsembl(biomart = "snps", dataset = "hsapiens_snp", mirror = 'uswest')
 
-# Step 2: Create a network in Cytoscape linking the SCPs to the genes
+# Create an empty data frame to store hits of rs with multiple genes associated
+multiple.hits <- data.frame('','')
+# Give this dataframe the same names, in order to combine then later
+names(multiple.hits) <- c('rsID', 'gene')
+
+# Obtain the associated gene for each variant and store in the data.frame
+for (rsnum in 1:nrow(variants)) {
+  # If there is 1 hit
+  if (dim(getBM(attributes = 'associated_gene', 
+                       filters = 'snp_filter', mart = variation, 
+                       values = variants$rsID[rsnum]))[1] == 1){
+  # Add this gene to the dataframe
+  variants$gene[rsnum] <- getBM(attributes = 'associated_gene', 
+                                 filters = 'snp_filter', mart = variation, 
+                                 values = variants$rsID[rsnum])[,1]
+  # If there are multiple hits
+  } else if (dim(getBM(attributes = 'associated_gene', 
+                       filters = 'snp_filter', mart = variation, 
+                       values = variants$rsID[rsnum]))[1] > 1){
+    # Find all unique hits
+    hits <-  getBM(attributes = 'associated_gene',
+           filters = 'snp_filter', mart = variation, 
+           values = variants$rsID[rsnum])[,1]
+    separated <- str_split(hits, ',')
+    combined <- unique(unlist(separated))
+    # Add the first hit to the data frame
+    variants$gene[rsnum] <- combined[1]
+    # Save the other hits to add after this loop
+    for (hit in 2:length(combined)) {
+      additional <- c(variants$rsID[rsnum], combined[hit])
+      names(additional) <- c('rsID', 'gene')
+      multiple.hits <- rbind(multiple.hits, additional)
+    }
+  }
+}
+
+# Combine the first hits and additional hits to obtain them all
+edge.list <- rbind(variants, multiple.hits)
+# Remove empty row
+edge.list <- edge.list[-which(edge.list$rsID == ""),]
+# Turn NA gene entry into empty entry
+edge.list[which(is.na(edge.list$gene)),] <- ""
+
+################################################################################
+
+## Step 2: Create a network in Cytoscape linking the SCPs to the genes
+
+# Connect to cytoscape (should be opened!)
+cytoscapePing()
+cytoscapeVersionInfo()
+
+# Create a dataframe with all rsIDs and genes
+nodes <- data.frame(id = c(unique(edge.list$rsID), unique(edge.list$gene)),
+                   group = c(rep('rs', length(unique(edge.list$rsID))),
+                             rep('gene', length(unique(edge.list$gene)))),
+                   stringsAsFactors = FALSE)
+# Create a dataframe to explain the edges
+edges <- data.frame(source = edge.list$rsID[-which(edge.list$gene == "")], 
+                    target = edge.list$gene[-which(edge.list$gene == "")],
+                    stringsAsFactors = FALSE)
+
+# Open the network in cytoscape
+createNetworkFromDataFrames(nodes, edges, title = "var and genes", 
+                            collection = "YB_AT2021.240")
